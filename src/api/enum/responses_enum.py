cfd2f93bb80504dff9eee3dc7966f987955f6619
Eduardo <eduardoeiflerf@gmail.com>
2026-06-02 10:18:52 -0300

    fix: merge com todos os responseEnum

--- src/api/enum/responses_enum.py
+++ src/api/enum/responses_enum.py
@@ -8,14 +8,11 @@ class ResponsesEnum(Enum):
     INSTITUTIONS_FETCHED = (status.HTTP_200_OK, "Successfully fetched institutions.")
     INSTITUTION_CREATED = (status.HTTP_201_CREATED, "Institution created successfully.")
     INSTITUTION_UPDATED = (status.HTTP_200_OK, "Institution updated successfully.")
-<<<<<<< HEAD
     INSTITUTION_DELETED = (status.HTTP_200_OK, "Institution deleted successfully.")
-=======
     MESSAGES_FETCHED = (status.HTTP_200_OK, "Successfully fetched messages.")
     MESSAGE_CREATED = (status.HTTP_201_CREATED, "Message created successfully.")
     ALL_USERS_VALID = (status.HTTP_200_OK, "All messages were sent successfully.")
     PARTIALLY_USERS_VALID = (status.HTTP_207_MULTI_STATUS, "Some messages could not be sent.")
->>>>>>> origin/dev
 
     # --- Error Responses (4xx) ---
     IP_NOT_ALLOWED = (status.HTTP_403_FORBIDDEN, "IP not allowed.")
@@ -29,12 +26,9 @@ class ResponsesEnum(Enum):
     FAILED_TO_FETCH_INSTITUTIONS = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch institutions.")
     FAILED_TO_UPDATE_INSTITUTION = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update institution.")
     FAILED_TO_CREATE_INSTITUTION = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create institution.")
-<<<<<<< HEAD
     FAILED_TO_DELETE_INSTITUTION = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to delete institution.")
-=======
     FAILED_TO_FETCH_MESSAGES = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch messages.")
     FAILED_TO_CREATE_MESSAGE = (status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create message.")
->>>>>>> origin/dev
 
     def __init__(self, status_code: int, message: str):
         self._status_code = status_code
